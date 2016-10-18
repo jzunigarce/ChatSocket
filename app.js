01b655af86969e6bac870e69b7d69ee2b514dfe7
Jesus Antonio Zuñiga Arce <jzunigarce@gmail.com>
2016-10-18 11:12:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
+app.use(express.static(__dirname + '/assets'));
+//Routes
 app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/chat', function(req, res){
   res.sendFile(__dirname + '/chat.html');
+});
+
+//Socket
+io.on('connection', function(socket){
+  console.log('A user connected');
+  socket.on('disconnect', function(){
+    console.log('User desconnected');
+  });
 });
 
 http.listen(3000, function(){
