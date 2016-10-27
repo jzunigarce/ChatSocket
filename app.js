7c24a7019fd610081ac29614ba464c154b59d907
Jesus Antonio Zuñiga Arce <jzunigarce@gmail.com>
2016-10-27 01:58:40 -0600

    Refactory source

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var users = [];
 app.use(express.static(__dirname + '/assets'));
 //Routes
 app.get('/', function(req, res){
@@ -12,9 +12,34 @@ app.get('/chat', function(req, res){
   res.sendFile(__dirname + '/chat.html');
 });
 
+var createUser = function(userName, socketId){
+  var colors = [];
+  for(var i = 0; i < 3; i++)
+    colors.push(Math.floor(Math.random() * 254));
+  return {'id': socketId, 'name': userName, 'color': colors.join(',')};
+}
+
+var isExists = function(userName){
+  var exists = false;
+  for(i in users){
+    if(users[i].name == userName){
+      return true;
+    }
+  }
+  return false;
+}
+
 //Socket
 io.on('connection', function(socket){
-  console.log('A user connected');
+  socket.on('signIn', function(userName){
+    var data;
+    if(!isExists(userName)){
+      data = createUser(userName, socket.id);
+      users.push(data);
+    }
+    console.log("Datos a enviar" + data);
+    socket.emit('signIn', data);
+  });
   socket.on('disconnect', function(){
     console.log('User desconnected');
   });
